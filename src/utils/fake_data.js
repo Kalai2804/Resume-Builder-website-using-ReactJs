ac0f1aa9e37b3c55eb6e6333c37f15b22d3e2d04
Kalai2804 <kalaiarasan2804@gmail.com>
2020-12-28 21:22:27 +0530

    asdw

---
+++ src/utils/fake_data.js
@@ -0,0 +1,50 @@
+//Data used when the user hits "Example" btn
+const data = {
+  header: {
+    name: "Kalai arasan",
+    address: "10-E type 1 qtrs block-22",
+    city: "Neyveli",
+    state: "Tamil nadu",
+    zip: "607807",
+    phone: "1234567890",
+    email: "kalaiarasan2804@email.com",
+    summary:
+      "To work in a challenging environment where I can put my best efforts and knowledge to reach the respective company’s goals and objectives.",
+  },
+  professional: {
+    company1: "Best enlist",
+    local1: "chennai",
+    position1: "intern",
+    start1: "nov/2020",
+    end1: "..",
+    desc1: [
+      "",
+      "Help others on their daily tasks.",
+      "",
+    ],
+    company2: "Your company",
+    local2: "Location",
+    position2: "Intern",
+    start2: "Jul/2012",
+    end2: "Dec/2014",
+    desc2: [
+      "description.",
+    ],
+  },
+  education: {
+    institution: "Jeppiaar institute of technology ",
+    city: "chennai",
+    major: "Bachelors in Finance",
+    gradYear: "2022",
+    additional: " (GPA 7.22)",
+  },
+  additional: [
+    "",
+    "English and Tamilspeaker",
+    "Adaptability",
+    "Interpersonal Communication",
+    "Friend of all",
+  ],
+};
+
+export default data;
